2cd59087368714889bf2deea9165a4dc895a3d9b
GRA1984BR <grazi1984.ss@gmail.com>
2024-09-23 02:04:00 -0300

    PrimeiroCommit

--- obj/Debug/net8.0/LHPet.AssemblyInfo.cs
+++ obj/Debug/net8.0/LHPet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LHPet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+222c357f54db3e10b0f30436d8563ca841976476")]
 [assembly: System.Reflection.AssemblyProductAttribute("LHPet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LHPet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
